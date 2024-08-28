e0bc8a3cd92a2bac851ffdc2cf49f71669f4057f
adlyatarisa <adlyatarisa@gmail.com>
2024-08-28 13:14:17 +0700

    fix: responsive navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -30,16 +30,16 @@ export default function Navbar({ className = '' }) {
     >
       <Link
         href={'/'}
-        className="flex scale-75 items-center ml-[85px] lg:scale-100"
+        className="flex scale-75 md:shrink-0 items-center md:ml-[85px] lg:scale-100"
       >
         <Image src="/navbar/Logo.svg" width={211} height={70} alt="logo" />
       </Link>
-      <div className="hidden lg:flex space-x-8 ml-[146px]">
+      <div className="hidden lg:flex space-x-8 lg:ml-[146px] ">
         {links.map((link) => (
           <Link
             key={link.href}
             href={link.href}
-            className={`text-lg ${getClassName(link.href)}`}
+            className={`text-lg ${getClassName(link.href)} hover:text-blue-primary`}
           >
             {link.text}
           </Link>
@@ -47,7 +47,7 @@ export default function Navbar({ className = '' }) {
       </div>
       <Link
         href="/get-started"
-        className="hidden lg:flex bg-blue-primary text-white py-3 px-6 rounded-xl mr-[30px]"
+        className="hidden lg:flex bg-blue-primary text-white py-3 px-6 md:px-5 md:ml-[15px] rounded-xl md:mr-[30px] hover:bg-[#7784ee]"
       >
         Get Started Free
       </Link>
@@ -55,7 +55,7 @@ export default function Navbar({ className = '' }) {
         <Image src="/navbar/menu-icon.svg" width={24} height={24} alt="Menu" />
       </button>
       {isOpen && (
-        <div className="lg:hidden absolute items-center justify-between top-full left-0 w-full bg-white shadow-lg flex flex-col items-start p-4 space-y-4">
+        <div className="2xl:hidden absolute items-center justify-between top-full left-0 w-full bg-white shadow-lg flex flex-col items-start p-4 space-y-4">
           {links.map((link) => (
             <Link
               key={link.href}
